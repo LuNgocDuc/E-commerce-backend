73bf0efd84f569b6310e155f18c1d958bcd71212
LuNgocDuc <lungocduc.horse@gmail.com>
2025-10-27 23:18:11 +0700

    Create Update User's information API

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -64,7 +64,30 @@ const loginUser = async (req, res) => {
     }
 }
 
+const updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id
+        const data = req.body
+        if(!userId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The userID is required '
+            })
+        }
+        console.log('userId', userId)
+        
+        const response = await UserService.updateUser(userId, data)
+        return res.status(200).json(response)
+    }
+    catch(e) {
+        return res.status(404).json({
+            massage: e
+        })
+    }
+}
+
 module.exports = {
     createUser,
     loginUser,
+    updateUser,
 }
